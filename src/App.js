76ed32be6d815a7cb3e48738d74c3d305ca7fe45
its-your-nuna <200103140@stu.sdu.edu.kz>
2022-06-24 16:54:18 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,83 +1,131 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import { TodoistApi } from '@doist/todoist-api-typescript'
 
-const BACKEND_URL = "http://10.65.132.54:3000";
+const api = new TodoistApi('0fbb9931fdc0180d30c59c2a8ec4626a8b50865d')
 
-/*
-* Plan:
-*   1. Define backend url
-*   2. Get items and show them +
-*   3. Toggle item done +
-*   4. Handle item add +
-*   5. Delete +
-*   6. Filter
-*
-* */
 
 function App() {
+  /*
+  
+    GET — получение ресурса
+    POST — создание ресурса
+    PUT — обновление ресурса
+    DELETE — удаление ресурса
+
+  */
   const [itemToAdd, setItemToAdd] = useState("");
   const [items, setItems] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [filterType, setFilterType] = useState('active')
+  const [isdone,setDone]= useState([])
+  const buttons = [
+    {
+      type: "active",
+      label: "Active",
+    },
+    {
+      type: "done",
+      label: "Done",
+    },
+  ];
+  const changevalue=()=>{
+    api.reopenTask(5464358139)
+    .then((isSuccess) => console.log(isSuccess))
+    .catch((error) => console.log(error))
+
+  };
+  useEffect(() => {
+    api.getTasks()
+      .then((tasks) => {
+        console.log(tasks)
+        setItems(tasks)
+      })
+    getAllItems();
+  
+  }, []);
+
 
   const handleChangeItem = (event) => {
     setItemToAdd(event.target.value);
   };
 
   const handleAddItem = () => {
-    axios.post(`${BACKEND_URL}/todos`, {
-        label:itemToAdd,
-        done: false
-    }).then((response) => {
-        setItems([ ...items, response.data])
+    api.addTask({
+      content: itemToAdd,
     })
+      .then((task) =>
+        setItems([...items, task]))
+      .catch((error) => console.log(error))
     setItemToAdd("");
   };
 
 
-  const toggleItemDone = ({ id, done }) => {
-      axios.put(`${BACKEND_URL}/todos/${id}`, {
-          done: !done
-      }).then((response) => {
+  const toggleItemDone = (item) => {
+
+    const { id, done, task_id } = item
+    console.log(id,done, task_id)
+
+    if(filterType==='done'){
+
+      api.reopenTask(task_id)
+      .then((isSuccess) => 
+      {
+      setItems([...items, item]);
+      setDone(isdone.filter((done) => {
+        return (done.id !== id) 
+      }))})
+      .catch((error) => console.log(error))
+    }else{
+      api.closeTask(id)
+      .then((isSuccess) => {
+        if (isSuccess) {
           setItems(items.map((item) => {
-              if (item.id === id) {
-                  return {
-                      ...item,
-                      done: !done
-                  }
+            if (item.id === id) {
+              return {
+                ...item,
+                done: !done
               }
-              return item
+            }
+            return item
           }))
-
-      })
-  };
-
-  // N => map => N
-    // N => filter => 0...N
-  const handleItemDelete = (id) => {
-      axios.delete(`${BACKEND_URL}/todos/${id}`).then((response) => {
-          const deletedItem = response.data;
-          console.log('Было:',items)
-          const newItems = items.filter((item) => {
-              return deletedItem.id !== item.id
-          })
-          console.log('Осталось:',newItems)
-          setItems(newItems)
-      })
-  };
-
-  useEffect(() => {
-      console.log(searchValue)
-      axios.get(`${BACKEND_URL}/todos/?filter=${searchValue}`).then((response) => {
-          setItems(response.data);
-      })
-  }, [searchValue])
+        }
+        console.log(isSuccess)
+      }
+      )
+      .catch((error) => console.log(error))
+    }
+   
+  }
 
 
+
+ 
+
+  const getAllItems = () => {
+    axios.get('https://api.todoist.com/sync/v8/completed/get_all', {
+      headers: {
+        Authorization:
+          "Bearer 0fbb9931fdc0180d30c59c2a8ec4626a8b50865d"
+      }
+    }).then((res)=>res.data)
+    .then((res)=>{
+      console.log(res.items)
+      setDone(res.items)
+    })
+  }
+
+  const handleFilterItems=(type)=>{
+    setFilterType(type)
+  }
+  const filterItem= filterType==='active'?items:
+  filterType==='done'?isdone:
+  items.filter((item)=>!item.done)
 
   return (
     <div className="todo-app">
-      {/* App-header */}
+      App-header
       <div className="app-header d-flex">
         <h1>Todo List</h1>
       </div>
@@ -92,34 +140,31 @@ function App() {
           onChange={(event) => setSearchValue(event.target.value)}
         />
       </div>
+      <div className="btn-group">
+        {buttons.map((item) => (
+          <button onClick={() => handleFilterItems(item.type)}
+            key={item.label}
+            type="button"
+            className={`btn btn-${filterType !== item.type ? "outline-" : ""
+              }info`}>
+            {item.label}
+          </button>
+        ))}
 
+      </div>
       {/* List-group */}
       <ul className="list-group todo-list">
+
         {items.length > 0 ? (
-          items.map((item) => (
+          filterItem.map((item) => (
             <li key={item.id} className="list-group-item">
-              <span className={`todo-list-item${item.done ? " done" : ""}`}>
+              <span className={`todo-list-item${item && item.done ? " done" : ""}`}>
                 <span
                   className="todo-list-item-label"
                   onClick={() => toggleItemDone(item)}
                 >
-                  {item.label}
+                  {item.content}
                 </span>
-
-                <button
-                  type="button"
-                  className="btn btn-outline-success btn-sm float-right"
-                >
-                  <i className="fa fa-exclamation" />
-                </button>
-
-                <button
-                  type="button"
-                  className="btn btn-outline-danger btn-sm float-right"
-                  onClick={() => handleItemDelete(item.id)}
-                >
-                  <i className="fa fa-trash-o" />
-                </button>
               </span>
             </li>
           ))
@@ -146,3 +191,36 @@ function App() {
 }
 
 export default App;
+// axios.get(`${api}`).then((response) => {
+//       console.log(response.data);
+//   })
+
+/*Неоднократно при создании веб-приложения вам может понадобиться
+ получать и отображать данные из API. наиболее популярным решением 
+ является использование axios, основанного на Promise HTTP-клиента.
+ Axios - очень популярная библиотека JavaScript, 
+ которую вы можете использовать для выполнения HTTP-запросов */
+
+/*API (Application programming interface) — это контракт, 
+который предоставляет программа. «Ко мне можно обращаться 
+так и так, я обязуюсь делать то и это».
+
+Контракт включает в себя:
+    саму операцию, которую мы можем выполнить,
+    данные, которые поступают на вход,
+    данные, которые оказываются на выходе (контент данных или сообщение об ошибке).
+ */
+/*
+
+  1. data - тело ответа, предоставленное сервером.
+   Если ответ сервера является JSON, Axios автоматически анализирует 
+   данные в объект JavaScript.
+  2.  status - код состояния HTTP из ответа, например 200 , 400 , 404 .
+  3.  statusText - сообщение о статусе HTTP из ответа сервера, 
+  например OK , Bad Request , Not Found .
+  4. headers - заголовки HTTP, сопровождающие ответ.
+  5.  config - конфигурация, предоставленная API Axios для запроса.
+  6.  request - Собственный запрос, который сгенерировал ответ. 
+  В Node.js это будет объект ClientRequest В браузере
+   это будет объект XMLHTTPRequest
+ */
